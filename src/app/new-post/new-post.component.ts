fdc97ef195e018ccbf63e4e510652946562e04e0
fadl <fadl@fadls-MacBook-Air.local>
2025-02-10 16:47:30 +0200

    vaild

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -3,13 +3,14 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 import {  HttpClientModule } from '@angular/common/http';
 import { PostService } from '../post.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Add this import
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css'],
   standalone: true,
-  imports: [ReactiveFormsModule, HttpClientModule]  
+  imports: [ReactiveFormsModule, HttpClientModule, CommonModule]  
 })
 export class NewPostComponent {
   postForm: FormGroup;
@@ -21,28 +22,33 @@ export class NewPostComponent {
     });
   }
 
+  ngOnInit() {
+    console.log('Initial Form Value:', this.postForm.value);
+    console.log('Initial Form Status:', this.postForm.status);
+  }
+
+  
   movePost(): void {
     this.router.navigate(['/posts']);
   }
-  onSubmit(): void {
-    if (this.postForm.valid) {
-      this.postService.addPost(this.postForm.value).subscribe({
-        next: (response) => {
-          console.log('Post created successfully', response);
-          alert('Post create successfully');
 
-          this.router.navigate(['/posts']);
-          // Handle further actions like redirection or display success message
-        },
-        error: (error) => {
-          
-          console.error('Post creation failed', error);
-          // Handle errors, show user feedback
-        }
-      });
-    } else {
-      this.postForm.markAllAsTouched();
-      console.log('Form is not valid');
-    }
+
+  onSubmit(): void {
+  if (this.postForm.valid) {
+    this.postService.addPost(this.postForm.value).subscribe({
+      next: (response) => {
+        console.log('Post created successfully', response);
+        alert('Post created successfully');
+        this.router.navigate(['/posts']);
+      },
+      error: (error) => {
+        console.error('Post creation failed', error);
+      }
+    });
+  } else {
+    this.postForm.markAllAsTouched();
+    console.log('Form is not valid');
+    console.log('Form Errors:', this.postForm.errors);
+  }
   }
 }
